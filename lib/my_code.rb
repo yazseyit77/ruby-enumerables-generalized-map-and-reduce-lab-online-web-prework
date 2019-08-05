c863924c9ea1478291508b1a54dc9854a3eb5fa8
Yazgeldi Seyidov <yazgeldi.seyidov@gmail.com>
2019-08-05 15:02:24 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -2,3 +2,15 @@
 def map(array)
   array.map{|n| n * -1}
 end
+
+def map(array)
+  array.map{|n| n}
+end
+
+def map(array)
+  array.map{|n| n * 2}
+end
+
+def map(array)
+  array.map{|n| n * n}
+end
