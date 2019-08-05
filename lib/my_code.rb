06cc88c1ecce44b5e36f18c33407b7463c2205ea
Yazgeldi Seyidov <yazgeldi.seyidov@gmail.com>
2019-08-05 14:55:50 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,10 +1,3 @@
 # Your Code Here
-def my_own_map(array)
-  new = []
-  i = 0
-  while i < array.length do
-    new.push(array[i]) * -1
-    i += 1
-  end
-  return new
-end
+map([array]){|n| n * -1}
+
